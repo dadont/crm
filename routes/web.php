d4be7ee9bb878bb2fd76dd0f262c4b29935772af
Your Name <you@example.com>
2022-07-16 00:18:36 +0300

    16/07/22-8test

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,15 @@ Route::get('/', [MainController::class, 'index']);
 Route::post('login', [AuthController::class, 'login']);
 Route::get('logout', [AuthController::class, 'logout']);
 
-Route::get('roles', [RolesController::class, 'index'])->middleware('auth');
+Route::middleware(['auth'])->group(function(){
+    Route::get('roles', [RolesController::class, 'index']);
+    Route::post('roles', [RolesController::class, 'create']);
+    Route::put('roles/{role}', [RolesController::class, 'update']);
+    Route::get('delete/{id}', [RolesController::class, 'delete']);    
+    Route::get('roles/{role}', [RolesController::class, 'show']);
+    Route::get('roles/{role}/users', [RolesController::class, 'users']);
 
-Route::post('roles', [RolesController::class, 'create']);
-Route::put('roles/{role}', [RolesController::class, 'update']);
-Route::get('delete/{id}', [RolesController::class, 'delete']);
+});
 
 
 
-Route::get('roles/{role}', [RolesController::class, 'show']);
-Route::get('roles/{role}/users', [RolesController::class, 'users']);
